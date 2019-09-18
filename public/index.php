036e3efcf10d0e11f04f223a53f30a5473e8da90
Alexandre Muraviov <amuraviov@globalia.ca>
2019-09-18 15:33:33 -0400

    move core outside of App

--- public/index.php
+++ public/index.php
@@ -1,18 +1,19 @@
 <?php
     define('PUBLIC_PATH', __DIR__);
     define('APPLICATION_PATH', __DIR__ . '/../App/');
+    define('CORE_PATH', __DIR__ . '/../core/');
     define('VIEWS_PATH', __DIR__ . '/../App/Views/');
 
     spl_autoload_register (function ($class_name) {
-        if (file_exists(APPLICATION_PATH . 'core/' . $class_name . '.php')) {
-            require_once APPLICATION_PATH . 'core/' . $class_name . '.php';
+        if (file_exists(CORE_PATH . $class_name . '.php')) {
+            require_once CORE_PATH . $class_name . '.php';
         } else if (file_exists(APPLICATION_PATH .  'Controllers/' . $class_name . '.php')) {
             require_once APPLICATION_PATH . 'Controllers/' . $class_name . '.php';
         }
     });
 
-    require_once APPLICATION_PATH . 'core/helpers.php';
-    require_once APPLICATION_PATH . 'core/routes.php';
+    require_once CORE_PATH . 'helpers.php';
+    require_once CORE_PATH . 'routes.php';
 
     $app = new App();
     $app->run();
