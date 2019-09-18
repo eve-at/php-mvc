7d7e66ddb31e1adcdcac29d3c1f332f4751fdeac
Alexandre Muraviov <amuraviov@globalia.ca>
2019-09-18 15:31:02 -0400

    rename includes to core

--- public/index.php
+++ public/index.php
@@ -4,15 +4,15 @@
     define('VIEWS_PATH', __DIR__ . '/../App/Views/');
 
     spl_autoload_register (function ($class_name) {
-        if (file_exists(APPLICATION_PATH . 'includes/' . $class_name . '.php')) {
-            require_once APPLICATION_PATH . 'includes/' . $class_name . '.php';
+        if (file_exists(APPLICATION_PATH . 'core/' . $class_name . '.php')) {
+            require_once APPLICATION_PATH . 'core/' . $class_name . '.php';
         } else if (file_exists(APPLICATION_PATH .  'Controllers/' . $class_name . '.php')) {
             require_once APPLICATION_PATH . 'Controllers/' . $class_name . '.php';
         }
     });
 
-    require_once APPLICATION_PATH . 'includes/helpers.php';
-    require_once APPLICATION_PATH . 'includes/routes.php';
+    require_once APPLICATION_PATH . 'core/helpers.php';
+    require_once APPLICATION_PATH . 'core/routes.php';
 
     $app = new App();
     $app->run();
