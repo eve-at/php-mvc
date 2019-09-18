3fda457e1d5f7b37895e2c19f468389ba0c5f9eb
Alexandre Muraviov <amuraviov@globalia.ca>
2019-09-18 15:34:29 -0400

    rename App to app

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
     define('PUBLIC_PATH', __DIR__);
-    define('APPLICATION_PATH', __DIR__ . '/../App/');
+    define('APPLICATION_PATH', __DIR__ . '/../app/');
     define('CORE_PATH', __DIR__ . '/../core/');
-    define('VIEWS_PATH', __DIR__ . '/../App/Views/');
+    define('VIEWS_PATH', __DIR__ . '/../app/Views/');
 
     spl_autoload_register (function ($class_name) {
         if (file_exists(CORE_PATH . $class_name . '.php')) {
